42035ad8a753dd50b3d5ed02cae1d23ac216d558
John Benedict Castroverde <jbthememer43@gmail.com>
2025-08-28 14:56:51 -0400

    added a second sun thats red

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -57,6 +59,13 @@ public class Picture
             sun.changeSize(80);
             sun.makeVisible();
             drawn = true;
+            
+            sun2.changeColor("red");
+            sun2.moveHorizontal(150);
+            sun2.moveVertical(40);
+            sun2.changeSize(50);
+            sun2.makeVisible();
+            drawn = true;
         }
     }
 
@@ -80,5 +89,6 @@ public class Picture
         window.changeColor("black");
         roof.changeColor("green");
         sun.changeColor("yellow");
+        sun2.changeColor("red");
     }
 }
